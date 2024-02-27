7ef873bf3b2d7b7e8581daae2876755e30468dec
DaviFaustino <davifaustino05@gmail.com>
2024-02-27 18:58:10 -0300

    chore: add project structure and settings

---
+++ src/main/java/com/davifaustino/productregistrationsystem/ProductRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.davifaustino.productregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davifaustino/productregistrationsystem/ProductRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davifaustino.productregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
