f3390363d8ffaed0d0c1f778b14bea14522138f5
DaviFaustino <davifaustino05@gmail.com>
2024-03-15 17:04:56 -0300

    feat: create category enum

---
+++ src/main/java/com/davifaustino/productregistrationsystem/entities/EnumCategories.java
@@ -0,0 +1,11 @@
+package com.davifaustino.productregistrationsystem.entities;
+
+public enum EnumCategories {
+    PANIFICAÇÃO,
+    BOBONS_E_SALGADINHOS,
+    BEBIDAS_E_LATICÍNIOS,
+    ALIMENTOS_REVENDA,
+    LIMPEZA_E_HIGIENE,
+    MATÉRIA_PRIMA,
+    OUTROS
+}
