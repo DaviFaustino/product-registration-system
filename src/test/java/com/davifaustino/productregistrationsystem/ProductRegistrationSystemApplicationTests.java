c8db8f70917efc4cfbf43521c95dc81f01fe3f5d
DaviFaustino <davifaustino05@gmail.com>
2024-03-15 20:35:44 -0300

    fix: correct the use of h2 as test db in profiles

--- src/test/java/com/davifaustino/productregistrationsystem/ProductRegistrationSystemApplicationTests.java
+++ src/test/java/com/davifaustino/productregistrationsystem/ProductRegistrationSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.davifaustino.productregistrationsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("devtest")
 class ProductRegistrationSystemApplicationTests {
 
 	@Test
