591849937d044604cc13d49116c851b2b0c46057
DaviFaustino <davifaustino05@gmail.com>
2024-03-29 18:30:56 -0300

    feat: create swagger documentation in the api

--- src/main/java/com/davifaustino/productregistrationsystem/ProductRegistrationSystemApplication.java
+++ src/main/java/com/davifaustino/productregistrationsystem/ProductRegistrationSystemApplication.java
@@ -3,7 +3,11 @@ package com.davifaustino.productregistrationsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product Registration System API", version = "1", description = "Backend API"))
 public class ProductRegistrationSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/davifaustino/productregistrationsystem/api/controllers/ProductTypeController.java
+++ src/main/java/com/davifaustino/productregistrationsystem/api/controllers/ProductTypeController.java
@@ -3,22 +3,31 @@ package com.davifaustino.productregistrationsystem.api.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.davifaustino.productregistrationsystem.api.dtos.ErrorResponseDto;
 import com.davifaustino.productregistrationsystem.api.dtos.ProductTypeDto;
 import com.davifaustino.productregistrationsystem.api.mappers.ProductTypeMapper;
 import com.davifaustino.productregistrationsystem.entities.ProductType;
 import com.davifaustino.productregistrationsystem.services.ProductTypeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/product-types")
+@RequestMapping(value = "/product-types", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "ProductTypes")
 public class ProductTypeController {
 
     @Autowired
@@ -27,6 +36,15 @@ public class ProductTypeController {
     @Autowired
     private ProductTypeMapper productTypeMapper;
 
+    @Operation(summary = "Create a new product type")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Product type saved successfully",
+                    content = {@Content(schema = @Schema(implementation =  ProductTypeDto.class))}),
+        @ApiResponse(responseCode = "400", description = "Invalid request content",
+                    content = {@Content(schema = @Schema(implementation =  ErrorResponseDto.class))}),
+        @ApiResponse(responseCode = "409", description = "Product type already exists in the database",
+                    content = {@Content(schema = @Schema(implementation =  ErrorResponseDto.class))})
+    })
     @PostMapping
     public ResponseEntity<ProductTypeDto> saveProductType (@RequestBody @Valid ProductTypeDto dto) {
         ProductType response = productTypeService.saveProductType(productTypeMapper.toEntity(dto));
