0570df4778f8f12dd243c44877117704b2ae9ed9
DaviFaustino <davifaustino05@gmail.com>
2024-04-21 12:37:31 -0300

    feat: create find methods in the repository

--- src/main/java/com/davifaustino/productregistrationsystem/repositories/ProductTypeRepository.java
+++ src/main/java/com/davifaustino/productregistrationsystem/repositories/ProductTypeRepository.java
@@ -1,11 +1,17 @@
 package com.davifaustino.productregistrationsystem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.davifaustino.productregistrationsystem.entities.EnumCategory;
 import com.davifaustino.productregistrationsystem.entities.ProductType;
 
 @Repository
 public interface ProductTypeRepository extends JpaRepository<ProductType, String> {
 
+    List<ProductType> findByNameIgnoreCaseContaining(String searchTerm);
+
+    List<ProductType> findByNameIgnoreCaseContainingAndCategory(String searchTerm, EnumCategory category);
 }
