b9303310300960969815009d025616c2c4292c23
DaviFaustino <davifaustino05@gmail.com>
2024-04-21 12:48:49 -0300

    feat: create get method in the service

--- src/main/java/com/davifaustino/productregistrationsystem/services/ProductTypeService.java
+++ src/main/java/com/davifaustino/productregistrationsystem/services/ProductTypeService.java
@@ -1,8 +1,12 @@
 package com.davifaustino.productregistrationsystem.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.davifaustino.productregistrationsystem.entities.EnumCategory;
 import com.davifaustino.productregistrationsystem.entities.ProductType;
 import com.davifaustino.productregistrationsystem.exceptions.RecordConflictException;
 import com.davifaustino.productregistrationsystem.repositories.ProductTypeRepository;
@@ -18,5 +22,13 @@ public class ProductTypeService {
             throw new RecordConflictException("The Product Type already exists in the database");
         }
         return productTypeRepository.save(productType);
+    }
+
+    public List<ProductType> getProductTypes(String searchTerm, Optional<EnumCategory> opCategory) {
+        if (opCategory.isPresent()) {
+
+            return productTypeRepository.findByNameIgnoreCaseContainingAndCategory(searchTerm, opCategory.get());
+        }
+        return productTypeRepository.findByNameIgnoreCaseContaining(searchTerm);
     }
 }
