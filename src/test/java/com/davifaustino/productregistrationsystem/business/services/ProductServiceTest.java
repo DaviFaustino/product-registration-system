f87165f23b80691326c1bdde863e3586de728cbc
DaviFaustino <davifaustino05@gmail.com>
2024-05-11 19:16:52 -0300

    test: create product service test

---
+++ src/test/java/com/davifaustino/productregistrationsystem/business/services/ProductServiceTest.java
@@ -0,0 +1,89 @@
+package com.davifaustino.productregistrationsystem.business.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.davifaustino.productregistrationsystem.business.entities.Product;
+import com.davifaustino.productregistrationsystem.business.exceptions.RecordConflictException;
+import com.davifaustino.productregistrationsystem.business.repositories.ProductRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+    @InjectMocks
+    ProductService productService;
+
+    Product product;
+
+    @BeforeEach
+    void setup() {
+        product = new Product();
+        product.setProductTypeName("");
+        product.setName("");
+        product.setDescription("");
+        product.setPurchasePriceInCents(1);
+        product.setPreviousPurchasePriceInCents(1);
+        product.setSalePriceInCents(1);
+        product.setPreviousSalePriceInCents(1);
+        product.setPriceUpdateDate(new Timestamp(1));
+        product.setFullStock(true);
+    }
+
+    @Test
+    @DisplayName("Must save a product to the database successfully")
+    void testSaveProduct1() {
+        product.setCode("1111111111111");
+
+        when(productRepository.existsById(any())).thenReturn(false);
+        when(productRepository.save(any(Product.class))).thenReturn(product);
+
+        Product productReturn = productService.saveProduct(product);
+
+        assertTrue(productReturn.equals(product));
+        verify(productRepository, times(0)).findByCodeContaining(any());
+        verify(productRepository).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("Must create a new code for the product and save it to the database successfully")
+    void testSaveProduct2() {
+        ArrayList<Product> productList = new ArrayList<>();
+        productList.add(new Product("          003", "", "", "", 1, 1, 1, 1, new Timestamp(1), true));
+
+        when(productRepository.existsById(any())).thenReturn(false);
+        when(productRepository.save(any(Product.class))).thenReturn(product);
+        when(productRepository.findByCodeContaining(any())).thenReturn(productList);
+
+        Product productReturn = productService.saveProduct(product);
+
+        assertTrue(productReturn.equals(product));
+        assertTrue(productReturn.getCode().equals("          004"));
+        verify(productRepository, times(1)).findByCodeContaining(any());
+        verify(productRepository).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("Must throw an error when trying to save a product to the database")
+    void testSaveProduct3() {
+        product.setCode("1111111111111");
+
+        when(productRepository.existsById(any())).thenReturn(true);
+
+        assertThrows(RecordConflictException.class, () -> productService.saveProduct(product));
+    }
+}
