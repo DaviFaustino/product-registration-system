8eb69bfbdc51ca2dea12ea8a38240d0973c61848
DaviFaustino <davifaustino05@gmail.com>
2024-05-14 20:12:29 -0300

    test: create test for ProductCode constraint anotation

---
+++ src/test/java/com/davifaustino/productregistrationsystem/api/validations/ProductCodeTest.java
@@ -0,0 +1,71 @@
+package com.davifaustino.productregistrationsystem.api.validations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+public class ProductCodeTest {
+
+    public class InnerTestClass {
+
+        @ProductCode
+        private String code;
+
+        public InnerTestClass(String code) {
+            this.code = code;
+        }
+    }
+
+    private InnerTestClass testClass;
+
+    @Test
+    @DisplayName("Validation must not be violated")
+    void productCodeTest1() {
+        testClass = new InnerTestClass("11111115");
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<InnerTestClass>> violations = validator.validate(testClass);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Validation must not be violated")
+    void productCodeTest2() {
+        testClass = new InnerTestClass("  11111610");
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<InnerTestClass>> violations = validator.validate(testClass);
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    @DisplayName("Validation must be violated")
+    void productCodeTest3() {
+        testClass = new InnerTestClass("11111116");
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<InnerTestClass>> violations = validator.validate(testClass);
+
+        assertFalse(violations.isEmpty());
+        assertTrue(violations.iterator().next().getMessage().equals("Invalid code"));
+    }
+
+    @Test
+    @DisplayName("Validation must be violated")
+    void productCodeTest4() {
+        testClass = new InnerTestClass("  a1111115");
+
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<InnerTestClass>> violations = validator.validate(testClass);
+
+        assertFalse(violations.isEmpty());
+        assertTrue(violations.iterator().next().getMessage().equals("The code must contain only numbers"));
+    }
+}
