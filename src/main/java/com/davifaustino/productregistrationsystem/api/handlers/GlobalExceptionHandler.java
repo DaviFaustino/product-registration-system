3b3fead3b9cfd36c4cddc066b11519ebeaa4310a
DaviFaustino <davifaustino05@gmail.com>
2024-05-19 22:28:55 -0300

    feat: add new exception handler
    Add new method in GlobalExceptionHandler to handle
    DataIntegrityViolationException.

--- src/main/java/com/davifaustino/productregistrationsystem/api/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/davifaustino/productregistrationsystem/api/handlers/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.davifaustino.productregistrationsystem.api.handlers;
 
 import java.sql.Timestamp;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -33,6 +34,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponseDto> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(e.getMessage(), new Timestamp(System.currentTimeMillis()), request.getRequestURI(), request.getMethod());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponseDto> handleDataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(e.getMessage(), new Timestamp(System.currentTimeMillis()), request.getRequestURI(), request.getMethod());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
