4c6f0efcfb72137bddf383b8c0ce10540231cadc
DaviFaustino <davifaustino05@gmail.com>
2024-05-24 22:24:45 -0300

    feat: create query to get product type names

--- src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductTypeRepository.java
+++ src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductTypeRepository.java
@@ -3,6 +3,7 @@ package com.davifaustino.productregistrationsystem.business.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.davifaustino.productregistrationsystem.business.entities.EnumCategory;
@@ -14,4 +15,9 @@ public interface ProductTypeRepository extends JpaRepository<ProductType, String
     List<ProductType> findByNameIgnoreCaseContaining(String searchTerm);
 
     List<ProductType> findByNameIgnoreCaseContainingAndCategory(String searchTerm, EnumCategory category);
+
+    @Query(value = "SELECT name from tb_product_types"
+                + " ORDER BY name ASC;",
+        nativeQuery = true)
+    List<String> findAllProductTypeNames();
 }
