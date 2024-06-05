b82e95e635be62078d3d4caa05a17f6582b521d5
DaviFaustino <davifaustino05@gmail.com>
2024-06-04 22:27:20 -0300

    feat: create methods in the ProductRepository

--- src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductRepository.java
+++ src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.davifaustino.productregistrationsystem.business.repositories;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,12 @@ import com.davifaustino.productregistrationsystem.business.entities.Product;
 public interface ProductRepository extends JpaRepository<Product, String> {
     
     ArrayList<Product> findByCodeContaining(String fragment);
+
+    Optional<Product> findByCode(String code);
+
+    List<Product> findByNameIgnoreCaseContaining(String name);
+
+    List<Product> findByNameIgnoreCaseContainingAndProductTypeName(String name, String productTypeName);
 
     boolean existsByName(String name);
 }
