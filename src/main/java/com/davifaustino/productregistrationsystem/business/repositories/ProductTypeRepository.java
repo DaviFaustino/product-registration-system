005dfaeb2155dedcce5ff652b7b8f69b85ebae65
DaviFaustino <davifaustino05@gmail.com>
2024-06-14 20:36:08 -0300

    feat: create method in the repository to update product type

--- src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductTypeRepository.java
+++ src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductTypeRepository.java
@@ -3,11 +3,15 @@ package com.davifaustino.productregistrationsystem.business.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.davifaustino.productregistrationsystem.business.entities.EnumCategory;
 import com.davifaustino.productregistrationsystem.business.entities.ProductType;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface ProductTypeRepository extends JpaRepository<ProductType, String> {
@@ -20,4 +24,12 @@ public interface ProductTypeRepository extends JpaRepository<ProductType, String
                 + " ORDER BY name ASC;",
         nativeQuery = true)
     List<String> findAllProductTypeNames();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE tb_product_types SET name = :#{#productTypeUpdates.name}, " +
+                   "category = :#{#productTypeUpdates.category.toString()}, " +
+                   "full_stock_factor = :#{#productTypeUpdates.fullStockFactor} " +
+                   "WHERE name = :name", nativeQuery = true)
+    int updateByName(@Param("name") String name, @Param("productTypeUpdates") ProductType productTypeUpdates);
 }
--- src/test/java/com/davifaustino/productregistrationsystem/business/repositories/ProductTypeRepositoryTest.java
+++ src/test/java/com/davifaustino/productregistrationsystem/business/repositories/ProductTypeRepositoryTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.davifaustino.productregistrationsystem.business.entities.EnumCategory;
+import com.davifaustino.productregistrationsystem.business.entities.ProductType;
+
 @DataJpaTest
 public class ProductTypeRepositoryTest {
 
@@ -23,5 +26,19 @@ public class ProductTypeRepositoryTest {
 
         assertEquals(Arrays.asList("Biscoito", "Danone", "Margarina", "Papel higiênico", "Pipoca", "Pão doce",
                                     "Reforçador", "Refri de 1L", "Vassoura", "Óleo"), names);
+    }
+
+    @Test
+    @DisplayName("Must update a product type successfully")
+    void testUpdateByName() {
+        ProductType productTypeUpdates = new ProductType("Pão adoçado", EnumCategory.PANIFICAÇÃO, 75, (short) 2);
+
+        productTypeRepository.updateByName("Pão doce", productTypeUpdates);
+        ProductType updatedProductType = productTypeRepository.findById("Pão adoçado").orElse(null);
+
+        assertNotNull(updatedProductType);
+        assertEquals((productTypeUpdates.getName()), updatedProductType.getName());
+        assertEquals(productTypeUpdates.getCategory(), updatedProductType.getCategory());
+        assertEquals(productTypeUpdates.getFullStockFactor(), updatedProductType.getFullStockFactor());
     }
 }
