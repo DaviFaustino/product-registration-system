8664da4b07aba65068338366abf196130d60cd4e
DaviFaustino <davifaustino05@gmail.com>
2024-06-19 19:25:40 -0300

    fix: fix toMap method

--- src/main/java/com/davifaustino/productregistrationsystem/api/mappers/ProductTypeMapper.java
+++ src/main/java/com/davifaustino/productregistrationsystem/api/mappers/ProductTypeMapper.java
@@ -1,5 +1,6 @@
 package com.davifaustino.productregistrationsystem.api.mappers;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,17 +12,12 @@ import org.springframework.stereotype.Component;
 import com.davifaustino.productregistrationsystem.api.dtos.requests.ProductTypeRequest;
 import com.davifaustino.productregistrationsystem.api.dtos.responses.ProductTypeResponse;
 import com.davifaustino.productregistrationsystem.business.entities.ProductType;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class ProductTypeMapper {
 
     @Autowired
     private ModelMapper modelMapper;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     public ProductType toEntity(ProductTypeRequest request) {
         return modelMapper.map(request, ProductType.class);
@@ -36,6 +32,11 @@ public class ProductTypeMapper {
     }
 
     public Map<String, Object> toMap(ProductTypeRequest request) {
-        return objectMapper.convertValue(request, new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", request.getName());
+        map.put("category", request.getCategory());
+        map.put("fullStockFactor", request.getFullStockFactor());
+
+        return map;
     }
 }
