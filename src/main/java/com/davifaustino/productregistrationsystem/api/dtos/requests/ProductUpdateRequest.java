fa0b49a08aabc8a401d1ec135c3261caf333e05e
DaviFaustino <davifaustino05@gmail.com>
2024-06-28 18:37:51 -0300

    feat: create dto to update product

---
+++ src/main/java/com/davifaustino/productregistrationsystem/api/dtos/requests/ProductUpdateRequest.java
@@ -0,0 +1,35 @@
+package com.davifaustino.productregistrationsystem.api.dtos.requests;
+
+import com.davifaustino.productregistrationsystem.api.validations.ProductCode;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductUpdateRequest {
+
+    @Size(min = 4, max = 13)
+    @ProductCode
+    String code;
+
+    @Size(max = 32)
+    String productTypeName;
+
+    @Size(min = 3, max = 42)
+    String name;
+
+    @Size(max = 256)
+    String description;
+
+    Integer purchasePriceInCents;
+
+    Integer salePriceInCents;
+
+    Boolean fullStock;
+}
