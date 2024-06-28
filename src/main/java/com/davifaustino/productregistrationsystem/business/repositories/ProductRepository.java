8c8742eefb52aab3e5263b42aff89ac11f5ea4c6
DaviFaustino <davifaustino05@gmail.com>
2024-06-28 18:31:55 -0300

    feat: create update method in ProductRepository

--- src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductRepository.java
+++ src/main/java/com/davifaustino/productregistrationsystem/business/repositories/ProductRepository.java
@@ -5,9 +5,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.davifaustino.productregistrationsystem.business.entities.Product;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
@@ -21,4 +26,18 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findByNameIgnoreCaseContainingAndProductTypeName(String name, String productTypeName);
 
     boolean existsByName(String name);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE tb_products SET product_type_name = :#{#productUpdates.productTypeName}, " +
+                    "name = :#{#productUpdates.name}, " +
+                    "description = :#{#productUpdates.description}, " +
+                    "purchase_price_in_cents = :#{#productUpdates.purchasePriceInCents}, " +
+                    "sale_price_in_cents = :#{#productUpdates.salePriceInCents}, " +
+                    "previous_purchase_price_in_cents = :#{#productUpdates.previousPurchasePriceInCents}, " +
+                    "previous_sale_price_in_cents = :#{#productUpdates.previousSalePriceInCents}, " +
+                    "price_update_date = :#{#productUpdates.priceUpdateDate}, " +
+                    "full_stock = :#{#productUpdates.fullStock} " +
+                    "WHERE code = :searchTerm", nativeQuery = true)
+    int updateByCode(@Param("searchTerm") String code, @Param("productUpdates") Product productUpdates);
 }
---
+++ src/test/java/com/davifaustino/productregistrationsystem/business/repositories/ProductRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.davifaustino.productregistrationsystem.business.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Timestamp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.davifaustino.productregistrationsystem.business.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    @DisplayName("Must update a product successfully")
+    void testUpdateByCodeOrName() {
+        Product productUpdates = new Product("          003", "Biscoito", "Nikito", "", 2, 1, 2, 1, new Timestamp(1719802800000l), true);
+
+        productRepository.updateByCode("          003", productUpdates);
+        Product updatedProduct = productRepository.findById("          003").orElse(null);
+
+        assertNotNull(updatedProduct);
+        assertEquals(productUpdates.getName(), updatedProduct.getName());
+        assertEquals(productUpdates.getPurchasePriceInCents(), updatedProduct.getPurchasePriceInCents());
+        assertEquals(productUpdates.getSalePriceInCents(), updatedProduct.getSalePriceInCents());
+        assertEquals(productUpdates.getPriceUpdateDate(), updatedProduct.getPriceUpdateDate());
+    }
+}
